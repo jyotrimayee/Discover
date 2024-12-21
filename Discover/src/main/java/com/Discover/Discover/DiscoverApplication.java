ca89284beb950a218840a7b758aec1e1dc13c9d5
ASD3@levisofttech.com <sethyjyotrimayee67@gmail.com>
2024-12-21 18:13:54 +0530

    initialcommit

---
+++ Discover/src/main/java/com/Discover/Discover/DiscoverApplication.java
@@ -0,0 +1,13 @@
+package com.Discover.Discover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverApplication.class, args);
+	}
+
+}
---
+++ Discover/src/test/java/com/Discover/Discover/DiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Discover.Discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
